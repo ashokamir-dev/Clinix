c2f5655213dde6b293b2610f64797f7408c505ee
ashokamir-dev <ashok.amir@outlook.com>
2025-11-29 19:01:32 +0000

    Add service worker for caching files

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE = 'clinicrun-v1';
+const FILES = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
